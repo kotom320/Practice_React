7965d5ac3ca2ac365ce9c67f2302e136b62fda48
tom <kotom320@gmail.com>
2020-01-18 14:00:31 +0900

    TOC Event, dataSet

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -9,8 +9,20 @@ class TOC extends Component{
         var data = this.props.data;
         var i =0;
         while(i<data.length) {
-        lists.push(<li key={data[i].id}><a href={'/content/'+data[i].id}>{data[i].title}</a></li>);
-        i = i+1;
+         lists.push(
+          <li key={data[i].id}> 
+           <a href={'/content/'+data[i].id}
+            data-id ={data[i].id}
+            onClick={function(e){
+
+               e.preventDefault();
+               this.props.OnChangePage(e.target.dataset.id);
+
+
+           }.bind(this)}
+           >{data[i].title}</a>
+          </li>);
+         i = i+1;
         }
       return(
         <nav>
