2e508a310a97a0061a15f29101fb67acb9c85c0b
tom <kotom320@gmail.com>
2020-01-19 19:11:08 +0900

    Delete

--- src/App.js
+++ src/App.js
@@ -115,9 +115,32 @@ class App extends Component {
 
       </TOC>
        <Control onChangeMode={function(_mode){
+         if(_mode === 'delete'){
+           if(window.confirm('Really??')){
+             var _contents = Array.from(this.state.contents);
+            var i = 0 ;
+            while(i<_contents.length){
+              if(_contents[i].id === this.state.selected_content_id){
+                _contents.splice(i,1);
+                break;
+              }
+              i=i+1;
+
+
+            }
+            this.setState({
+              mode:'welcome',
+              contents:_contents
+            });
+            alert('deleted');
+           }
+         }else{
+
+         
           this.setState({
             mode:_mode
-          })
+          });
+        }
        }.bind(this)}></Control>
 
       {this.getContent()}
